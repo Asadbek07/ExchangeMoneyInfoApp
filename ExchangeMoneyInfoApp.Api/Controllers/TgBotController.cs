28da818b295512ee6df3c80a2f514ace1756b98f
Asadbek Sindarov <asindarov07@gmail.com>
2022-05-25 10:26:56 +0500

    Add Infrastructure for TgBot

---
+++ ExchangeMoneyInfoApp.Api/Controllers/TgBotController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Telegram.Bot.Types;
+
+namespace ExchangeMoneyInfoApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/bot")]
+    public class TgBotController : ControllerBase
+    {
+        private readonly ITelegramService telegramService;
+
+        public TgBotController(ITelegramService telegramService)
+        {
+            this.telegramService = telegramService;
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromBody] object update)
+        {
+            var upd = JsonConvert.DeserializeObject<Update>(update.ToString());
+
+            try
+            {
+                await this.telegramService.ExecuteAsync(upd);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
